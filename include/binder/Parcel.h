a68b2a9186ecb145b22550a8a032d98c8bd5b656
Alberto García <garcia98ftw@gmail.com>
2014-07-05 13:38:34 +0000

    Fix build
    Change-Id: Ia53ed08fe2a0f3cdbb3341caeb0c5a41e4e461cf

--- include/binder/Parcel.h
+++ include/binder/Parcel.h
@@ -102,11 +102,9 @@ public:
     status_t            writeStrongBinder(const sp<IBinder>& val);
     status_t            writeWeakBinder(const wp<IBinder>& val);
     status_t            writeInt32Array(size_t len, const int32_t *val);
-#ifdef STE_HARDWARE
-    status_t            write(const Flattenable& val);
-#else
     status_t            writeByteArray(size_t len, const uint8_t *val);
-#endif
+
+    status_t            write(const Flattenable& val);
 
     template<typename T>
     status_t            write(const LightFlattenable<T>& val);

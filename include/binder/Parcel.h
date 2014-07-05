b21512b5acad88cab1c07c1f4d2cdeb199768016
Alberto García <garcia98ftw@gmail.com>
2014-07-05 13:37:39 +0000

    Revert "Fix build"
    This reverts commit 4ef94387d56e037c052f53344cfdadd8ddcbe9e1.

--- include/binder/Parcel.h
+++ include/binder/Parcel.h
@@ -102,10 +102,14 @@ public:
     status_t            writeStrongBinder(const sp<IBinder>& val);
     status_t            writeWeakBinder(const wp<IBinder>& val);
     status_t            writeInt32Array(size_t len, const int32_t *val);
+#ifdef STE_HARDWARE
     status_t            write(const Flattenable& val);
+#else
     status_t            writeByteArray(size_t len, const uint8_t *val);
+#endif
 
-    status_t            write(const Flattenable& val);
+    template<typename T>
+    status_t            write(const LightFlattenable<T>& val);
 
 
     // Place a native_handle into the parcel (the native_handle's file-
